0c5333302b657f9adae01a60cd1b706230e983b4
Roman Nitzsche <roman.nitzsche@etu.unistra.fr>
2025-12-23 16:48:33 +0100

    fix(bitscan): fixed LSB table

--- src/moves/bit_scan.hpp
+++ src/moves/bit_scan.hpp
@@ -2,20 +2,35 @@
 
 #include <cstdint>
 
+#if defined(__GNUC__) || defined(__clang__)
 inline int bitscan_forward(uint64_t bb) {
     if (bb == 0) return -1;
-    
+    return __builtin_ctzll(bb);
+}
+#elif defined(_MSC_VER)
+#include <intrin.h>
+inline int bitscan_forward(uint64_t bb) {
+    if (bb == 0) return -1;
+    unsigned long index;
+    _BitScanForward64(&index, bb);
+    return (int)index;
+}
+#else
+inline int bitscan_forward(uint64_t bb) {
+    if (bb == 0) return -1;
+
     static const int index64[64] = {
-        0, 47,  1, 56, 48, 27,  2, 60,
-        57, 49, 41, 37, 28, 16,  3, 61,
-        54, 58, 35, 52, 50, 42, 21, 44,
-        38, 32, 29, 23, 17, 11,  4, 62,
-        46, 55, 26, 59, 40, 36, 15, 53,
-        34, 51, 20, 43, 31, 22, 10, 45,
-        25, 39, 14, 33, 19, 30,  9, 24,
-        13, 18,  8, 12,  7,  6,  5, 63
+         0,  1, 48,  2, 57, 49, 28,  3,
+        61, 58, 50, 42, 38, 29, 17,  4,
+        62, 55, 59, 36, 53, 51, 43, 22,
+        45, 39, 33, 30, 24, 18, 12,  5,
+        63, 47, 56, 27, 60, 41, 37, 16,
+        54, 35, 52, 21, 44, 32, 23, 11,
+        46, 26, 40, 15, 34, 20, 31, 10,
+        25, 14, 19,  9, 13,  8,  7,  6
     };
     
     const uint64_t debruijn64 = 0x03f79d71b4cb0a89ULL;
     return index64[((bb & -bb) * debruijn64) >> 58];
 }
+#endif
